fb1a74c793622c99dc177330236642f9bb4d8eb1
SWI Field Reports <sindhjirasoi@gmail.com>
2026-06-22 18:35:59 +0530

    Embed Drive video player in report viewer

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "swi-reports-v16";
+const CACHE = "swi-reports-v17";
 const APP_SHELL = ["/", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg", "/swi-logo.png"];
 
 self.addEventListener("install", (event) => {
